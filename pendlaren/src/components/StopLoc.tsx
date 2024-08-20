bf3e5fa5b9cf9166ad42f132bf0fdf3fbccdc217
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2024-08-20 14:53:19 +0200

    Added code

--- pendlaren/src/components/StopLoc.tsx
+++ pendlaren/src/components/StopLoc.tsx
@@ -14,7 +14,7 @@ function StopLoc(props: StopLocProps) {
     const response =
       await fetch(`https://api.resrobot.se/v2.1/departureBoard?id=${
         stop.extId
-      }&maxJourneys=20&format=json&accessId=${import.meta.env.VITE_API_KEY}
+      }&duration=10&format=json&accessId=${import.meta.env.VITE_API_KEY}
 `);
     const { Departure } = await response.json();
     console.log(Departure);
